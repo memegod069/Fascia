806d6db67a151fcf1da17194c1727fc6563740b9
meme god <memegod069052@gmail.com>
2026-07-09 18:27:28 +0530

    M2: Add helper script to focus and zoom viewport camera onto simulated biceps

---
+++ tests/zoom_to_biceps.py
@@ -0,0 +1,27 @@
+import bpy
+
+# Find the simulated biceps object
+biceps = bpy.data.objects.get("Biceps_Simulated")
+
+if not biceps:
+    print("Could not find Biceps_Simulated object. Make sure you ran the import script first.")
+else:
+    # Delete all other objects in the scene to avoid confusion
+    for obj in list(bpy.data.objects):
+        if obj.name != "Biceps_Simulated":
+            bpy.data.objects.remove(obj, do_unlink=True)
+            
+    # Select the biceps and make it active
+    bpy.ops.object.select_all(action='DESELECT')
+    biceps.select_set(True)
+    bpy.context.view_layer.objects.active = biceps
+    
+    # Zoom the viewport camera to fit the biceps object
+    for area in bpy.context.screen.areas:
+        if area.type == 'VIEW_3D':
+            for region in area.regions:
+                if region.type == 'WINDOW':
+                    with bpy.context.temp_override(area=area, region=region, active_object=biceps, selected_objects=[biceps]):
+                        bpy.ops.view3d.view_selected()
+                        
+    print("Viewport successfully zoomed and focused on the Biceps simulation!")
